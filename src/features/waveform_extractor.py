eded176911bb6103831bc2ed2e9920bc24dac760
Anderson Silva <andsf@live.com>
2023-10-27 22:24:43 -0300

    refactor: change waveform crop function and dataset default folders

--- src/features/waveform_extractor.py
+++ src/features/waveform_extractor.py
@@ -9,21 +9,24 @@ class WaveformExtractor(Extractor):
     WaveformExtractor
     =======
 
-    Extracts the Waveform information from a dataset.
+    Extracts the Waveform information from a dataset. 
 
     Args:
         dataset (Dataset): The dataset that contains the audio.
         audio_column (str): The name of the audio column
-        time_fixed (int): The fixed length of the waveform.
-        name (str): The name of the extractor
+        time_fixed (int): The fixed time duration of the waveform. When crop_length is not 0, 
+        the waveform is cropped.
+        crop_length (int): The length of the waveform. When time_fixed is not 0, the waveform 
+        is fixed. crop_length has more priority than time_fixed.
     """
 
-    def __init__(self, dataset: Dataset, column="audio", time_fixed=0):
+    def __init__(self, dataset: Dataset, column="audio", time_fixed=0, crop_length=0):
         """Initializes a new WaveformExtractor
         """
         super().__init__(dataset=dataset, column=column, name="Waveform")
 
         self.time_fixed = time_fixed
+        self.crop_length = crop_length
 
     def __getitem__(self, idx):
         # Get the audio data
@@ -32,13 +35,30 @@ class WaveformExtractor(Extractor):
         # Audio Data Keys = 'path', 'array', and 'sampling_rate'
         waveform = audio_data['array']
 
-        if self.time_fixed != 0:
+        if self.crop_length != 0:
+            waveform = self.__crop(waveform, self.crop_length)
+        elif self.time_fixed != 0:
             sample_size = self.time_fixed * audio_data['sampling_rate']
+            waveform = self.__crop(waveform, sample_size)
 
-            if waveform.size()[0] < sample_size:
-                pad = (0, sample_size - waveform.size()[0])
-                waveform = torch.nn.functional.pad(waveform, pad, value=0)
-            else:
-                waveform = waveform[:sample_size]
+        return waveform
+
+    def __crop(self, waveform, length):
+        """
+        Crop the waveform to a specified length.
+
+        Parameters:
+            waveform (torch.Tensor): The input waveform.
+            length (int): The desired length of the waveform.
+
+        Returns:
+            torch.Tensor: The cropped waveform.
+        """
+
+        if waveform.size()[0] < length:
+            pad = (0, length - waveform.size()[0])
+            waveform = torch.nn.functional.pad(waveform, pad, value=0)
+        else:
+            waveform = waveform[:length]
 
         return waveform
