5f6faadbea7d4b53248aee6655cc72b39c4ebc16
neliareis <neliareis@tecgraf.puc-rio.br>
2023-11-17 18:43:09 -0300

    Save the chart

--- src/visualization/charts_viewer.py
+++ src/visualization/charts_viewer.py
@@ -9,7 +9,7 @@ class ChartsViewer:
     """
 
     @staticmethod
-    def get_line_chart(values, title="Line Chart", x_label="X", y_label="Y"):
+    def get_line_chart(values, title="Line Chart", x_label="X", y_label="Y", path=None):
         """
         Displays a line chart with the specified values, title, and labels.
 
@@ -18,10 +18,15 @@ class ChartsViewer:
             title (str): The title of the chart. Defaults to "Line Chart".
             x_label (str): The label for the x-axis. Defaults to "X".
             y_label (str): The label for the y-axis. Defaults to "Y".
+            path (str): The path to save the chart. Defaults to None.
         """
         plt.plot(range(1, len(values) + 1), values, marker='o')
         plt.xlabel(x_label)
         plt.ylabel(y_label)
         plt.title(title)
         plt.grid()
-        plt.show()
+        
+        if path is not None:
+            plt.savefig(f"{path}/{title}.png")
+        else:
+            plt.show()
